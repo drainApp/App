afd7ac0d065c0438ac6c5988c34122ac875fcc8b
JaeHyun Kim <carbitna03@gmail.com>
2020-12-21 01:37:53 +0900

    complete configuration page

--- public/js/configuration.js
+++ public/js/configuration.js
@@ -2,9 +2,10 @@ var slider = document.getElementById("myRange");
 var output;
 output = slider.value;				// 처음에 0
             
-slider.oninput = function() {
-    output= this.value;	
-    console.log(output)			// 슬라이더 움직일 때 값에 따라 다르게 들어가게
+slider.oninput = function() {// 슬라이더 움직일 때 값에 따라 다르게 들어가게
+    this.value=4
+    alert('음악을 들을 수 없는 환경이라면 게임을 플레이하지 말아주세요')
+    	
 }
 
 var ear = document.getElementById('ear')
