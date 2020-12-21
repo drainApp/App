ebfc4f69d9362cb6870ca25e606ae2ab42e1e8be
JaeHyun Kim <carbitna03@gmail.com>
2020-12-21 20:44:13 +0900

    RWD

--- public/js/configuration.js
+++ public/js/configuration.js
@@ -1,12 +1,11 @@
 var slider = document.getElementById("myRange");
-var output;
-output = slider.value;				// 처음에 0
+var output = slider.value;				// 처음에 0
             
-slider.oninput = function() {// 슬라이더 움직일 때 값에 따라 다르게 들어가게
-    this.value=4
+slider.onmouseup = function() {// 슬라이더 움직일 때 값에 따라 다르게 들어가게
     alert('음악을 들을 수 없는 환경이라면 게임을 플레이하지 말아주세요')
-    	
+    this.value=4
 }
+
 
 var ear = document.getElementById('ear')
 var slash = document.getElementById('slash')
