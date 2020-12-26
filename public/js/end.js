6e911504b93358fbef832ceccfaa0943084dc278
JaeHyun Kim <carbitna03@gmail.com>
2020-12-26 22:26:59 +0900

    end img brightness

--- public/js/end.js
+++ public/js/end.js
@@ -1,6 +1,23 @@
 body = document.getElementsByTagName('body')[0]
-var ck=2
+var ck=25
 var s
+var flag=false
+var start= function(){
+    s = setInterval(function(){
+    if(!flag){
+        ck+=5
+        if(ck==10000)flag=true
+    }
+    // else{ 
+    //     ck-=1
+    //     if(ck==25)flag=false
+    // }
+    body.style.filter = "brightness(" + ck+"%)"
+    console.log(ck)
+},50)
+}
+start()
+
 
 var bgm = new Audio('');
 var bgm_url = "./assets/end/Kontrol.mp3";
@@ -8,18 +25,4 @@ bgm = new Audio(bgm_url);
 if(localStorage.getItem('configComplete') == 'true'){
     bgm.volume = localStorage.getItem('volume');
     bgm.play();
-}
-
-var start= function(){
-    s = setInterval(function(){
-    var imgurl = "url('../assets/end/Ending" +ck+ ".png')"
-    ck++
-    body.style.backgroundImage = imgurl
-    if(ck==8)stop()
-},500)
-}
-start()
-
-stop=function(){
-    clearInterval(s)
 }
