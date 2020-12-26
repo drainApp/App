16ba53ec80b368f21c78bd595d03d137bb9cce82
JaeHyun Kim <carbitna03@gmail.com>
2020-12-26 23:31:42 +0900

    ending scene

--- public/js/end.js
+++ public/js/end.js
@@ -1,13 +1,28 @@
-body = document.getElementsByTagName('body')[0]
+var end = document.getElementsByClassName('ending')[0]
+var imgg = document.getElementsByClassName('i')[0]
+
 var ck=25
 var s
+var flag = false 
 var start= function(){
     s = setInterval(function(){
-    if(ck==10000)stop()
-    if(ck<1800)ck+=10
-    else ck+=15
-    body.style.filter = "brightness(" + ck+"%)"
-    console.log(ck)
+    if(!flag){
+        if(ck>1500){
+            ck=0.01
+            imgg.style.display="none"
+            flag=true
+        }
+        else if(ck<800)ck+=7
+        else ck+=12
+        imgg.style.filter = "brightness(" + ck+"%)"
+        console.log(ck)
+    }
+    if(flag){
+        end.style.opacity = ck
+        ck+=0.008
+        console.log(ck)
+        if(ck>1)stop()
+    }
 },50)
 }
 start()
